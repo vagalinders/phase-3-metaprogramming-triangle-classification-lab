93ac463134d77d6125381f9e572a3b1d56153146
Chris Mann <chrispmann@gmail.com>
2022-10-12 16:53:43 -0700

    commit tree 5d53f035d10e29a5f88262adcf88d8d430f89ae0

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if @a == @b && @b == @c
+      :equilateral
+    elsif 
+      @a == @b || @b == @c || @a == @c
+      :isosceles
+    elsif @a != @b && @b != @c && @a != @c
+      :scalene
+    
+    elsif @a <= 0 || @b <= 0 || @c <= 0 || @a + @b > @c || @c + @a > @b
+      TriangleError
+    
+    else 
+      TriangleError
+    end
+  end
+
+    class TriangleError < StandardError
+      "Invalid triangle!"
+    end
 end
